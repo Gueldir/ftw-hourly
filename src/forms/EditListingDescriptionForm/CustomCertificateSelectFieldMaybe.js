1210ed92d0fa7ae03d28e7f97e3c7e1fd4fab236
Rémy Tribhout <gueldir@gmail.com>
2020-04-15 02:21:33 +0200

    Adding category list placeholder
    And required validator and translation.

--- src/forms/EditListingDescriptionForm/CustomCertificateSelectFieldMaybe.js
+++ src/forms/EditListingDescriptionForm/CustomCertificateSelectFieldMaybe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FieldSelect } from '../../components';
+import { required } from '../../util/validators';
 
 import css from './EditListingDescriptionForm.css';
 
@@ -8,9 +9,20 @@ const CustomCertificateSelectFieldMaybe = props => {
   const certificateLabel = intl.formatMessage({
     id: 'EditListingDescriptionForm.certificateLabel',
   });
+  const categoryPlaceholder = intl.formatMessage({
+    id: 'EditListingDescriptionForm.certificatePlaceholder',
+  });  
+  const categoryRequired = required(
+    intl.formatMessage({
+      id: 'EditListingDescriptionForm.certificateRequired',
+    })
+  );
 
   return certificate ? (
-    <FieldSelect className={css.certificate} name={name} id={id} label={certificateLabel}>
+    <FieldSelect className={css.certificate} name={name} id={id} label={certificateLabel} validate={categoryRequired}>
+      <option disabled value="">
+        {categoryPlaceholder}
+      </option>
       {certificate.map(c => (
         <option key={c.key} value={c.key}>
           {c.label}
