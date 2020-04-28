3674b8c17d2677c77da0eea2ff3c02a491c946ba
Rémy Tribhout <gueldir@gmail.com>
2020-04-29 00:11:22 +0200

    Removed certificate from listing

--- src/components/EditListingDescriptionPanel/EditListingDescriptionPanel.js
+++ src/components/EditListingDescriptionPanel/EditListingDescriptionPanel.js
@@ -49,14 +49,14 @@ const EditListingDescriptionPanel = props => {
       <h1 className={css.title}>{panelTitle}</h1>
       <EditListingDescriptionForm
         className={css.form}
-        initialValues={{ title, description, certificate: publicData.certificate, category: publicData.category  }}
+        initialValues={{ title, description, /*certificate: publicData.certificate,*/ category: publicData.category  }}
         saveActionMsg={submitButtonText}
         onSubmit={values => {
-          const { title, description, certificate, category } = values;
+          const { title, description, /*certificate,*/ category } = values;
           const updateValues = {
             title: title.trim(),
             description,
-            publicData: { certificate, category },
+            publicData: { /*certificate,*/ category },
           };
 
           onSubmit(updateValues);
@@ -67,7 +67,7 @@ const EditListingDescriptionPanel = props => {
         updated={panelUpdated}
         updateInProgress={updateInProgress}
         fetchErrors={errors}
-        certificate={config.custom.certificate}
+        //certificate={config.custom.certificate}
         category={config.custom.category}
       />
     </div>
