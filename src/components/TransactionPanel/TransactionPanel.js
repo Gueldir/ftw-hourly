8c0527deff4eb7c18708042fd35e8534b3deb894
Rémy Tribhout <gueldir@gmail.com>
2020-05-05 15:15:11 +0200

    Mobile and desktop for meeting

--- src/components/TransactionPanel/TransactionPanel.js
+++ src/components/TransactionPanel/TransactionPanel.js
@@ -104,6 +104,7 @@ export class TransactionPanelComponent extends Component {
     this.onSendMessageFormBlur = this.onSendMessageFormBlur.bind(this);
     this.onMessageSubmit = this.onMessageSubmit.bind(this);
     this.scrollToMessage = this.scrollToMessage.bind(this);
+    this.mobileAndTabletCheck = this.mobileAndTabletCheck.bind(this);
   }
 
   componentDidMount() {
@@ -119,7 +120,9 @@ export class TransactionPanelComponent extends Component {
   }
 
   onOpenMeetingModal() {
-    this.setState({ isMeetingModalOpen: true });
+    let isMobile = this.mobileAndTabletCheck();
+    if (!isMobile)
+      this.setState({ isMeetingModalOpen: true });
   }
 
   componentWillUnmount() {  
@@ -182,6 +185,12 @@ export class TransactionPanelComponent extends Component {
       });
     }
   }
+
+  mobileAndTabletCheck() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  };
 
   render() {
     const {
@@ -330,6 +339,7 @@ export class TransactionPanelComponent extends Component {
     const isSessionStarted = minutes <= 5 && this.state.date < bookingRange.end ? false : true;
     
     const loadMeetingRoom = () => {
+      const isMobile = this.mobileAndTabletCheck();
       const roomId = currentProvider.attributes.profile.abbreviatedName + currentProvider.id.uuid.substring(currentProvider.id.uuid.length - 5, currentProvider.id.uuid.length);
       const roomPwd = currentTransaction.attributes.protectedData.roomPwd;
       const userName = currentUser.attributes.profile.displayName;
@@ -342,14 +352,16 @@ export class TransactionPanelComponent extends Component {
           interfaceConfigOverwrite: {
           filmStripOnly: false,
           SHOW_JITSI_WATERMARK: false,
-          jwt: moderator
+          jwt: moderator,
         },
         configOverwrite: {
+          disableKick: true,
+          doNotStoreRoom: true,
           disableSimulcast: false,
           enableWelcomePage: true,
           enableUserRolesBasedOnToken: true,
           enableFeaturesBasedOnToken: true,
-          //lockRoomGuestEnabled: false,
+          lockRoomGuestEnabled: true,
           /*localRecording: {
             enabled: true,
             format: 'flac',
@@ -383,18 +395,23 @@ export class TransactionPanelComponent extends Component {
         }
       };
 
-      const api = new window.JitsiMeetExternalAPI(domain, options);
-      api.on('passwordRequired', () => {
-        api.executeCommand('password', roomPwd);
-      });
-      api.on('videoConferenceJoined', () => {
-        api.executeCommand('subject', roomName);
-        api.executeCommand('password', roomPwd);
-      });
-      api.on('readyToClose', () => {
-        this.setState({ isMeetingModalOpen: false });
-        api.dispose();
-      });
+      if (isMobile) {
+        const intent = "intent://meet.jit.si/"+encodeURI(options.roomName)+"#jitsi_meet_external_api_id=0&config.disableSimulcast=false&&config.doNotStoreRoom=true&config.enableWelcomePage=true&config.enableUserRolesBasedOnToken=true&config.enableFeaturesBasedOnToken=true&interfaceConfig.TOOLBAR_ALWAYS_VISIBLE=false&interfaceConfig.SHOW_JITSI_WATERMARK=false&interfaceConfig.SHOW_WATERMARK_FOR_GUESTS=true&interfaceConfig.SHOW_BRAND_WATERMARK=false&interfaceConfig.SHOW_POWERED_BY=false&interfaceConfig.APP_NAME=%22Virtualhub%20session%22&interfaceConfig.NATIVE_APP_NAME=%22Virtualhub%20session%22&interfaceConfig.PROVIDER_NAME=%22"+encodeURI(options.interfaceConfigOverwrite.PROVIDER_NAME)+"%22&interfaceConfig.LANG_DETECTION=true&interfaceConfig.INVITATION_POWERED_BY=true&interfaceConfig.TOOLBAR_BUTTONS=%5B%22microphone%22%2C%22camera%22%2C%22closedcaptions%22%2C%22desktop%22%2C%22fullscreen%22%2C%22fodeviceselection%22%2C%22hangup%22%2C%22chat%22%2C%22recording%22%2C%22sharedvideo%22%2C%22settings%22%2C%22videoquality%22%2C%22tileview%22%2C%22download%22%2C%22help%22%2C%22mute-everyone%22%2C%22localrecording%22%5D&interfaceConfig.SETTINGS_SECTIONS=%5B%22devices%22%2C%22language%22%2Cnull%5D&interfaceConfig.VIDEO_LAYOUT_FIT=%22both%22&interfaceConfig.filmStripOnly=false&interfaceConfig.HIDE_KICK_BUTTON_FOR_GUESTS=true&userInfo.displayName=%22"+encodeURI(options.userInfo.displayName)+"%22#Intent;scheme=org.jitsi.meet;package=org.jitsi.meet;end"
+        window.location.replace(intent);
+      } else {
+        const api = new window.JitsiMeetExternalAPI(domain, options);
+        api.on('passwordRequired', () => {
+          api.executeCommand('password', roomPwd);
+        });
+        api.on('videoConferenceJoined', () => {
+          api.executeCommand('subject', roomName);
+          api.executeCommand('password', roomPwd);
+        });
+        api.on('readyToClose', () => {
+          this.setState({ isMeetingModalOpen: false });
+          api.dispose();
+        });
+      }
     }
 
     const saleButtons = (
@@ -417,7 +434,7 @@ export class TransactionPanelComponent extends Component {
         showButtons={stateData.showMeetingButton}
         onOpenMeetingModal={this.onOpenMeetingModal}
         loadMeetingRoom={loadMeetingRoom}
-        buttonsDisabled={isSessionStarted}
+        buttonsDisabled={false}
       />
     );
 
